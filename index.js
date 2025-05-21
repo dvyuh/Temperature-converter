d67cdb4feb80631162f5fa6a232690b937d3f508
Dvyuh <dvyuh.dev@gmail.com>
2025-05-21 09:44:06 +0530

    changed text to txt

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const textbox = document.getElementById("textbox");
+const txtbox = document.getElementById("textbox");
 const tofa = document.getElementById("tofa");
 const toce = document.getElementById("toce");
 const result = document.getElementById("result");
@@ -6,11 +6,11 @@ let temp;
 
 function convert() {
   if (tofa.checked) {
-    temp = Number(textbox.value);
+    temp = Number(txtbox.value);
     temp = (temp * 9) / 5 + 32;
     result.textContent = temp.toFixed(1) + "°F";
   } else if (toce.checked) {
-    temp = Number(textbox.value);
+    temp = Number(txtbox.value);
     temp = (temp - 32) * (5 / 9);
     result.textContent = temp.toFixed(1) + "°C";
   } else {
