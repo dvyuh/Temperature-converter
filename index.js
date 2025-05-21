c175d691fe98921a4b587104a75188a07b733271
Dvyuh <dvyuh.dev@gmail.com>
2025-05-21 10:27:02 +0530

    txt tot text

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const txtbox = document.getElementById("txtbox");
+const textbox = document.getElementById("textbox");
 const tofa = document.getElementById("tofa");
 const toce = document.getElementById("toce");
 const result = document.getElementById("result");
@@ -6,11 +6,11 @@ let temp;
 
 function convert() {
   if (tofa.checked) {
-    temp = Number(txtbox.value);
+    temp = Number(textbox.value);
     temp = (temp * 9) / 5 + 32;
     result.textContent = temp.toFixed(1) + "°F";
   } else if (toce.checked) {
-    temp = Number(txtbox.value);
+    temp = Number(textbox.value);
     temp = (temp - 32) * (5 / 9);
     result.textContent = temp.toFixed(1) + "°C";
   } else {
